fa47e18b139b299445f8473f9e381c98238454e5
Markeh <31129951+M4rk3h@users.noreply.github.com>
2019-01-16 10:26:55 +0000

    Added buttons.

--- BayView1/BayView1/ManagerView.Designer.cs
+++ BayView1/BayView1/ManagerView.Designer.cs
@@ -29,22 +29,62 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ManagerView));
+            this.btnCustomers = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnBookings = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // btnCustomers
+            // 
+            this.btnCustomers.Location = new System.Drawing.Point(58, 75);
+            this.btnCustomers.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnCustomers.Name = "btnCustomers";
+            this.btnCustomers.Size = new System.Drawing.Size(180, 180);
+            this.btnCustomers.TabIndex = 4;
+            this.btnCustomers.Text = "Customers";
+            this.btnCustomers.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "welcome label";
+            // 
+            // btnBookings
+            // 
+            this.btnBookings.Location = new System.Drawing.Point(313, 75);
+            this.btnBookings.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnBookings.Name = "btnBookings";
+            this.btnBookings.Size = new System.Drawing.Size(180, 180);
+            this.btnBookings.TabIndex = 5;
+            this.btnBookings.Text = "Bookings";
+            this.btnBookings.UseVisualStyleBackColor = true;
             // 
             // ManagerView
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(533, 325);
+            this.ClientSize = new System.Drawing.Size(711, 400);
+            this.Controls.Add(this.btnCustomers);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnBookings);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "ManagerView";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "ManagerView";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnCustomers;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnBookings;
     }
 }
