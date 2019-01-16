21e381898e63aa3a4d7d27b75539d96c5de52a51
Markeh <31129951+M4rk3h@users.noreply.github.com>
2019-01-16 19:48:31 +0000

    Struggling with Chart.

--- BayView1/BayView1/Analytics.Designer.cs
+++ BayView1/BayView1/Analytics.Designer.cs
@@ -31,26 +31,32 @@
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
             System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            this.staffChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.Chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.btnClose = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.staffChart)).BeginInit();
+            this.rbStaff = new System.Windows.Forms.RadioButton();
+            this.rbSales = new System.Windows.Forms.RadioButton();
+            this.rbRooms = new System.Windows.Forms.RadioButton();
+            this.rbBreakfast = new System.Windows.Forms.RadioButton();
+            this.rbDisability = new System.Windows.Forms.RadioButton();
+            this.rbMonth = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.Chart1)).BeginInit();
             this.SuspendLayout();
             // 
-            // staffChart
+            // Chart1
             // 
             chartArea1.Name = "ChartArea1";
-            this.staffChart.ChartAreas.Add(chartArea1);
+            this.Chart1.ChartAreas.Add(chartArea1);
             legend1.Name = "Legend1";
-            this.staffChart.Legends.Add(legend1);
-            this.staffChart.Location = new System.Drawing.Point(12, 32);
-            this.staffChart.Name = "staffChart";
+            this.Chart1.Legends.Add(legend1);
+            this.Chart1.Location = new System.Drawing.Point(12, 12);
+            this.Chart1.Name = "Chart1";
             series1.ChartArea = "ChartArea1";
             series1.Legend = "Legend1";
             series1.Name = "Series1";
-            this.staffChart.Series.Add(series1);
-            this.staffChart.Size = new System.Drawing.Size(658, 300);
-            this.staffChart.TabIndex = 0;
-            this.staffChart.Text = "chart1";
+            this.Chart1.Series.Add(series1);
+            this.Chart1.Size = new System.Drawing.Size(658, 300);
+            this.Chart1.TabIndex = 0;
+            this.Chart1.Text = "chart1";
             // 
             // btnClose
             // 
@@ -65,25 +71,105 @@
             this.btnClose.UseVisualStyleBackColor = true;
             this.btnClose.Click += new System.EventHandler(this.btnExit_Click);
             // 
+            // rbStaff
+            // 
+            this.rbStaff.AutoSize = true;
+            this.rbStaff.Location = new System.Drawing.Point(12, 330);
+            this.rbStaff.Name = "rbStaff";
+            this.rbStaff.Size = new System.Drawing.Size(58, 21);
+            this.rbStaff.TabIndex = 5;
+            this.rbStaff.TabStop = true;
+            this.rbStaff.Text = "Staff";
+            this.rbStaff.UseVisualStyleBackColor = true;
+            this.rbStaff.CheckedChanged += new System.EventHandler(this.rbStaff_CheckedChanged);
+            // 
+            // rbSales
+            // 
+            this.rbSales.AutoSize = true;
+            this.rbSales.Location = new System.Drawing.Point(12, 363);
+            this.rbSales.Name = "rbSales";
+            this.rbSales.Size = new System.Drawing.Size(64, 21);
+            this.rbSales.TabIndex = 5;
+            this.rbSales.TabStop = true;
+            this.rbSales.Text = "Sales";
+            this.rbSales.UseVisualStyleBackColor = true;
+            // 
+            // rbRooms
+            // 
+            this.rbRooms.AutoSize = true;
+            this.rbRooms.Location = new System.Drawing.Point(94, 330);
+            this.rbRooms.Name = "rbRooms";
+            this.rbRooms.Size = new System.Drawing.Size(73, 21);
+            this.rbRooms.TabIndex = 5;
+            this.rbRooms.TabStop = true;
+            this.rbRooms.Text = "Rooms";
+            this.rbRooms.UseVisualStyleBackColor = true;
+            // 
+            // rbBreakfast
+            // 
+            this.rbBreakfast.AutoSize = true;
+            this.rbBreakfast.Location = new System.Drawing.Point(94, 363);
+            this.rbBreakfast.Name = "rbBreakfast";
+            this.rbBreakfast.Size = new System.Drawing.Size(89, 21);
+            this.rbBreakfast.TabIndex = 5;
+            this.rbBreakfast.TabStop = true;
+            this.rbBreakfast.Text = "Breakfast";
+            this.rbBreakfast.UseVisualStyleBackColor = true;
+            // 
+            // rbDisability
+            // 
+            this.rbDisability.AutoSize = true;
+            this.rbDisability.Location = new System.Drawing.Point(200, 330);
+            this.rbDisability.Name = "rbDisability";
+            this.rbDisability.Size = new System.Drawing.Size(85, 21);
+            this.rbDisability.TabIndex = 5;
+            this.rbDisability.TabStop = true;
+            this.rbDisability.Text = "Disability";
+            this.rbDisability.UseVisualStyleBackColor = true;
+            // 
+            // rbMonth
+            // 
+            this.rbMonth.AutoSize = true;
+            this.rbMonth.Location = new System.Drawing.Point(200, 363);
+            this.rbMonth.Name = "rbMonth";
+            this.rbMonth.Size = new System.Drawing.Size(78, 21);
+            this.rbMonth.TabIndex = 5;
+            this.rbMonth.TabStop = true;
+            this.rbMonth.Text = "Monthly";
+            this.rbMonth.UseVisualStyleBackColor = true;
+            // 
             // Analytics
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(682, 403);
+            this.Controls.Add(this.rbMonth);
+            this.Controls.Add(this.rbBreakfast);
+            this.Controls.Add(this.rbDisability);
+            this.Controls.Add(this.rbRooms);
+            this.Controls.Add(this.rbSales);
+            this.Controls.Add(this.rbStaff);
             this.Controls.Add(this.btnClose);
-            this.Controls.Add(this.staffChart);
+            this.Controls.Add(this.Chart1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Analytics";
             this.Text = "Analytics";
             this.Load += new System.EventHandler(this.Analytics_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.staffChart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Chart1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataVisualization.Charting.Chart staffChart;
+        private System.Windows.Forms.DataVisualization.Charting.Chart Chart1;
         private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.RadioButton rbStaff;
+        private System.Windows.Forms.RadioButton rbSales;
+        private System.Windows.Forms.RadioButton rbRooms;
+        private System.Windows.Forms.RadioButton rbBreakfast;
+        private System.Windows.Forms.RadioButton rbDisability;
+        private System.Windows.Forms.RadioButton rbMonth;
     }
 }
--- BayView1/BayView1/Analytics.cs
+++ BayView1/BayView1/Analytics.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Windows.Forms.DataVisualization.Charting;
+using System.Data.SQLite;
 
 namespace BayView1
 {
@@ -19,6 +20,14 @@ namespace BayView1
             //Start in center position
             this.StartPosition = FormStartPosition.CenterScreen;
         }
+
+        //Details of the database
+        string dbDetails = dbConnection.dbsource;
+        //Details for Datatable (display) & DataAdapter (Pull+Push)
+        SQLiteConnection dbCon;
+        //Declare Series
+        Series rooms;
+        private string sql1;
 
         private void btnExit_Click(object sender, EventArgs e)
         {
@@ -36,12 +45,53 @@ namespace BayView1
 
         private void Analytics_Load(object sender, EventArgs e)
         {
-            staffChart.Titles.Clear();
-            staffChart.Titles.Add("Total Staff Members");
-            staffChart.ChartAreas[0].AxisX.IsMarginVisible = false;
-            staffChart.ChartAreas[0].AxisX.Maximum = 10;
-            staffChart.ChartAreas[0].AxisX.Minimum = 1;
-            staffChart.ChartAreas[0].AxisX.Title="Staff";
+            //Set properties for the Chart.
+            Chart1.Titles.Clear();
+            Chart1.Titles.Add("Total Staff Members");
+            Chart1.ChartAreas[0].AxisX.IsMarginVisible = false;
+            Chart1.ChartAreas[0].AxisX.Maximum = 10;
+            Chart1.ChartAreas[0].AxisX.Minimum = 1;
+            Chart1.ChartAreas[0].AxisX.Title = "Staff";
+
+        }
+
+        private void rbStaff_CheckedChanged(object sender, EventArgs e)
+        {
+            //Clear the chart.
+            Chart1.Series.Clear();
+            //Add a new series
+            rooms = Chart1.Series.Add("Singles");
+            rooms.Color = Color.DarkBlue;
+            //Declare SQL
+            sql1 = @"Select * From Staff;";
+            
+            //Try to connect.
+            //try
+            //{
+            //    using (SQLiteCommand cmd = new SQLiteCommand(sql1, dbCon))
+            //    {
+            //        dbCon.Open();
+            //        using 
+            //        {
+            //            int element = 0;
+            //            while (dr.Read())
+            //            {
+            //                rooms.Points.AddXY(element, Convert.ToInt32(dr[0]));
+            //                Chart1.Series[0].Points[element].AxisLabel = dr[1].ToString();
+            //                Chart1.ResetAutoValues();
+            //                Chart1.Update();
+            //                element++;
+            //            }
+            //        }
+            //        dbCon.Close();
+
+            //    }
+            //}
+
+            //catch
+            //{
+            //    MessageBox.Show("Error on Radio Button");
+            //}
         }
     }
 }
